928aef79ff678ee5be625b587d2a58c03a1ca9a4
tonye-d <itztonye@gmail.com>
2017-11-12 22:26:58 +0100

    modified some things

--- assets/js/day-5.js
+++ assets/js/day-5.js
@@ -1,16 +1,16 @@
 const panels = document.querySelectorAll(".panel");
 
 function toggleOpen() {
-    panels.forEach(panel => panel.classList.remove("open"));
+    //panels.forEach(panel => panel.classList.remove("open"));
     this.classList.toggle("open");
 }
 
 function toggleactive(e) {
     if (e.propertyName.includes("flex")) {
-        panels.forEach(panel => panel.classList.remove("open-active"));
+        //panels.forEach(panel => panel.classList.remove("open-active"));
         this.classList.toggle("open-active");
     }
 }
 
-panels.forEach(panel => panel.addEventListener("mouseover", toggleOpen));
+panels.forEach(panel => panel.addEventListener("click", toggleOpen));
 panels.forEach(panel => panel.addEventListener("transitionend", toggleactive));
